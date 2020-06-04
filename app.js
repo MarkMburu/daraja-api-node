d21ccb8cbe0794e7c16c223e3f664ebfc24265b1
MarkMburu <markmburu685@gmail.com>
2020-06-04 08:45:45 +0300

    initial commit

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const request = require('request');
+const middleware = require('./src/middleware');
+
 require('dotenv').config();
 
 const app = express();
@@ -7,10 +9,16 @@ const app = express();
 const port = process.env.PORT || 8000;
 
 app.get('/',(req,res)=>{
-  res.send('Hello daraja!!!')
+  res.status(200).json({
+    message:'Hello daraja!!!'
+  })
 });
 
-app.get('/access_token',(req,res)=>{
+app.get('/access_token',access,(req,res)=>{
+  res.status(200).json({access_token: req.access_token})
+});
+
+function access(req,res,next){
   // access token here
   url = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials"
   auth = "Basic " + new Buffer(process.env.CONSUMER_KEY + ":" + process.env.CONSUMER_SECRET).toString("base64");
@@ -26,10 +34,15 @@ app.get('/access_token',(req,res)=>{
         console.log(error)
       }
       else{
-        res.status(200).json(body)
+        req.access_token = JSON.parse(body).access_token;
+        next()
       }
     })
-});
+}
+
+
+app.use(middleware.notFound);
+app.use(middleware.errorHandler);
 
 app.listen(port,(err,live)=>{
   if(err){
